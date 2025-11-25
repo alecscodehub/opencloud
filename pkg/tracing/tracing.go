bc0d0c533b0dae934f5a29e9d5b4444b82e738db
Jörn Friedrich Dreyer <jfd@butonic.de>
2025-11-25 09:29:45 +0100

    bump pkg/tracing semconf to 1.37.0
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -15,7 +15,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
